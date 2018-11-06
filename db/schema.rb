8895d5257553128be59a1f43de9911fd13f55850
lalogf <lalogf@gmail.com>
2018-11-06 10:49:09 -0500

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -216,8 +216,8 @@ ActiveRecord::Schema.define(version: 20181008050747) do
     t.string "full_name"
     t.string "phone_number"
     t.string "password_digest"
-    t.datetime "device_created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "device_updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "device_created_at", default: -> { "now()" }, null: false
+    t.datetime "device_updated_at", default: -> { "now()" }, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "otp", null: false
